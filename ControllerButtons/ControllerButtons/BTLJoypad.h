b6419970f45ba676a10dbfc31ab39cd32f7c04b6
Michael Buckley <thebuckley@gmail.com>
2014-03-06 18:46:12 -0800

    Added Joypad Config object

--- ControllerButtons/ControllerButtons/BTLJoypad.h
+++ ControllerButtons/ControllerButtons/BTLJoypad.h
@@ -2,18 +2,18 @@
 //  BTLJoypad.h
 //  ControllerButtons
 //
-//  Created by Buckley on 3/4/14.
+//  Created by Buckley on 3/6/14.
 //  Copyright (c) 2014 Michael Buckley. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@class BTLInput;
+@class BTLJoypadConfig;
 
 @interface BTLJoypad : NSObject
 
-- (instancetype) initWithName: (NSString*) name;
-
-@property (nonatomic, readwrite, copy) NSString* name;
+@property (nonatomic, readwrite, copy) NSString*        name;
+@property (nonatomic, readonly,  copy) NSArray*         configs;
+@property (nonatomic, readwrite, weak) BTLJoypadConfig* selectedConfig;
 
 @end
---
+++ ControllerButtons/ControllerButtons/BTLJoypadConfig.h
@@ -0,0 +1,19 @@
+//
+//  BTLJoypad.h
+//  ControllerButtons
+//
+//  Created by Buckley on 3/4/14.
+//  Copyright (c) 2014 Michael Buckley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BTLInput;
+
+@interface BTLJoypadConfig : NSObject
+
+- (instancetype) initWithName: (NSString*) name;
+
+@property (nonatomic, readwrite, copy) NSString* name;
+
+@end
